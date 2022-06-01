1bc55a34f690c8c996f1f105df4a5529407abb98
dmkur <fgameua@gmail.com>
2022-06-01 19:33:51 +0300

    les2

--- lessons/lesson-2/app.js
+++ lessons/lesson-2/app.js
@@ -1,2 +1,13 @@
 const express = require('express')
 
+// turn on express wit app
+const app = express()
+
+app.get('/', (req, res) => {
+
+})
+
+// server starts listening app on PORT
+app.listen(5000, () => {
+    console.log('App listen 5000')
+})
