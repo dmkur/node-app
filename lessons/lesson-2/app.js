49913e15feb7cbed19bbeb87022e6423792b9b24
dmkur <fgameua@gmail.com>
2022-06-05 19:06:37 +0300

    les-2

--- lessons/lesson-2/app.js
+++ lessons/lesson-2/app.js
@@ -7,8 +7,30 @@ const app = express()
 // the main routes:
 // GET, POST, PUT, HEAD, DELETE, PATCH, OPTIONS, CONNECT, TRACE
 app.get('/', (req, res) => {
-    console.log(req)
-    res.end('end')
+//    req - what we need
+//    res - what we give
+
+    // res.end('end response')
+    // res.send('<h1>send response</h1>')
+    // res.json({response: 'json'})
+
+    // res.write('hello ')
+    // res.write('world')
+    // res.end()
+
+    res.status(404).end('Status not found')
+// status code
+// Informational responses ( 100–199 )
+// Successful responses ( 200–299 )
+// Redirects ( 300–399 )
+// Client errors ( 400–499 )
+// Server errors ( 500–599 )
+})
+
+app.get('/users', (req, res) => {
+    res.json(
+        [{name: 'Dima'}, {name:'Lol'}]
+    )
 })
 
 // server starts listening app on PORT
