b30cea79c6beeeecb259055885d3ca865fa5da09
dmkur <fgameua@gmail.com>
2022-06-12 15:24:17 +0300

    hw end 100%

--- homeworks/02-hw/app.js
+++ homeworks/02-hw/app.js
@@ -5,41 +5,44 @@ const path = require('path')
 
 const usersDB = require('./db/users')
 
-
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({defaultLayout: false}))
 app.set('views', path.join(__dirname, 'static'))
 
-app.get('/ping', (req, res) => {
-    res.end('pong')
+app.get('/', (req, res) => {
+    res.redirect('/login')
 })
 
 app.get('/users', (req, res) => {
     res.render('users', {usersDB})
 })
+
 app.get('/reg', (req, res) => {
     res.render('reg')
 })
-app.post('/newUser', (req, res) => {
+
+app.post('/sign_up', (req, res) => {
+    const pathToSave = path.join(__dirname, 'db', 'users.js')
     const {name, password, email, age} = req.body
     //console.log(name, password, email, age)
-   const newUser = usersDB.push({name: name, password: password, email: email, age:Number(age)})
-    const newdata = [...usersDB, newUser]
-    const newPath = path.join(__dirname, 'db', 'users.js')
-    fs.writeFile(newPath, JSON.stringify(`module.exports = ${newdata}`), err => {
-        if(err){
-            console.log(e)
+
+    usersDB.push({name: name, password: password, email: email, age: Number(age)})
+    const newUser = JSON.stringify(usersDB)
+    //console.log(newUser)
+
+    fs.writeFile(pathToSave, `module.exports = ${newUser}`, err => {
+        if (err) {
+            console.log(err)
             return
         }
         console.log('New user add')
     })
-    console.log(newdata)
     res.redirect('/login')
 })
 
@@ -54,16 +57,13 @@ app.post('/auth', (req, res) => {
     const currentUserPass = usersDB.find(i => i.password === password)
     // console.log(currentUser)
 
-    if(currentUserEmail && currentUserPass){
+    if (!currentUserEmail || !currentUserPass) {
+        res.render('bad_login')
+    }
+    if (currentUserEmail && currentUserPass) {
         res.redirect('/users')
-    } if(!currentUserEmail) {
-        res.redirect('/reg')
     }
 })
-
-
-
-
 
 app.listen(5000, () => {
     console.log('App listen 5000')
--- homeworks/02-hw/db/users.js
+++ homeworks/02-hw/db/users.js
@@ -1,6 +1,3 @@
-module.exports = [
-    {password: 'Pa$$w0rd1', email: "lol1@lol", name: 'Vika', age: 1},
-    {password: 'Pa$$w0rd2', email: "lol2@lol", name: 'Dima', age: 1},
-    {password: 'Pa$$w0rd3', email: "lol3@lol", name: 'Oleg', age: 1},
-    {password: 'Pa$$w0rd4', email: "lol43@lol", name: 'Ira', age: 1},
-]
+module.exports = [{"name":"Anna","password":"lol1994","email":"Anna@lol1994","age":19},
+    {"name":"Nikol","password":"astro2022","email":"as","age":22},
+    {"name":"Diana","password":"lol190","email":"lisa@don.ua","age":24}]
